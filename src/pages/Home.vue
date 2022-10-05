27fa687cfe60a0148a1bf66e9615753f485ab97f
Kanhapong <112235436+Kanhapong@users.noreply.github.com>
2022-10-05 21:10:26 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <navbar />
+  <section class="home">
+
+    <div class="image">
+        <img src="images/Lx.png" alt="">
+    </div>
+
+    <div class="content">
+        <h3>hi, i am Kanhapong Kanhakul</h3>
+        <span>Belive the Impossible is Possible.</span>
+        <p>Hi!</p>
+        <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+    </div>
+
+</section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  components: { Navbar },
+
+}
+</script>
+
+<style>
+
+</style>
